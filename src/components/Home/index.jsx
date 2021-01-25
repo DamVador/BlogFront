e6b2f4b2edbcac635a973fefdc66d0d1c9b0fc78
Damien <damienbanville@hotmail.fr>
2021-01-25 21:10:09 +0100

    url to fetch okay

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
-const Navbar = () => {
+const Home = () => {
+    const [data, setData] = useState([])
+    
+    const homeFetch = () => {
+        fetch(`http://localhost:3000/api/articles`, {
+        "method": "GET",
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        })
+        .then((response) => {
+        return response.json()
+        })
+        .then((response) => {
+            console.log(response)
+        // const orderedResponse = response.data
+        //     .sort((a, b) => (b.attributes.created_at).localeCompare((a.attributes.created_at)))
+        // const cropData = orderedResponse.slice(0, 12)
+        // setData(cropData)
+        // console.log(data)
+        }).catch(error => {
+        console.log(error)
+        })
+    }
 
+    useEffect(() => {
+        homeFetch()
+      }, [])
+    
+    
     return (
         <div>
             pouet
@@ -9,4 +37,4 @@ const Navbar = () => {
     );
 }
 
-export default Navbar
+export default Home

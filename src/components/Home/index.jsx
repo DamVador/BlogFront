1c45aa28a74aa8eabbda4bd8681175a315960090
Damien <damienbanville@hotmail.fr>
2021-01-31 17:56:40 +0100

    some worked done on the cursor

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import './index.css';
+import image1 from './1.jpeg';
 
 const Home = () => {
     const [data, setData] = useState([])
@@ -15,6 +17,10 @@ const Home = () => {
         })
         .then((response) => {
             console.log(response)
+            setData(response)
+            console.log('data updated')
+            console.log(data)
+            console.log(data[0].title)
         // const orderedResponse = response.data
         //     .sort((a, b) => (b.attributes.created_at).localeCompare((a.attributes.created_at)))
         // const cropData = orderedResponse.slice(0, 12)
@@ -29,10 +35,24 @@ const Home = () => {
         homeFetch()
       }, [])
     
+      const onMouseMove = (num) => {
+          console.log(num)
+            const cursor = document.querySelector(".cursor")
+            //cursor.style.left = `${e.pageX}px`
+            //cursor.style.top = `${e.pageY}px`
+            cursor.style.backgroundColor = "green"
+           // cursor.style.cursor = `url(src/assets/${num}.jpeg), auto`
+           cursor.style.cursor = "url(./1.jpeg), auto"
+            //cursor.style.cursor = 'url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/9632/meh.png), auto'
+            //cursor.style.cursor = "pointer"
+        }
     
     return (
-        <div>
-            pouet
+        <div className="cursor">
+             {data.map(elt => <h1 className={`${elt.title[elt.title.length-1]}`} onMouseOver={() => onMouseMove(elt.title[elt.title.length-1])}>{elt.title}</h1>)}
+             {console.log("laa")}
+             {data.map(elt => console.log(elt.title[elt.title.length-1]))}
+             <img src={image1} />
         </div>
     );
 }

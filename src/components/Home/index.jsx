dd8ed1a5f942c0ed722207daef8de8dc3ae6110a
Damien <damienbanville@hotmail.fr>
2021-02-04 19:54:35 +0100

    div changing without issue

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './index.css';
+import './index.scss';
 import image1 from './1.jpg';
 import image2 from './2.jpg';
 import image3 from './3.jpg';
@@ -9,9 +9,9 @@ const Home = () => {
     const [data, setData] = useState([])
     const [cursorX, setCursorX] = useState()
     const [cursorY, setCursorY] = useState()
-    const [imageName, setImageName] = useState()
-
-
+    const boxRef = React.useRef(null);
+    const [background, setBackground] = useState();
+    const [isHovering, setIsHovering] = useState(false);
     window.addEventListener('mousemove', (e) => {
         setCursorX(e.pageX - 100)
         setCursorY(e.pageY - 150)
@@ -59,40 +59,77 @@ const Home = () => {
         event()
       }, [])
 
+
       useEffect(() => {
-      }, [imageName])
+      }, [background])
+
     const tabImg = []
     tabImg.push(image1)
     tabImg.push(image2)
     tabImg.push(image3)
     tabImg.push(image4)
 
-    const changeImage = (num) => {
+    const changeImage = (e, num) => {
         const cursor = document.querySelector(".cursor")
         console.log("DANS MOUSE OVER")
-        console.log(num)
-       // setImageName("image"+num)
-        cursor.style.backgroundImage = `url(${tabImg[num-1]})`
+        //console.log(e.clientX, e.clientY)
+       // setCurrentImage(tabImg[num-1])
+       //cursor.style.backgroundImage = `url(${tabImg[num-1]})`
+       setBackground(tabImg[num-1])
     }
 
+    const onmouseout = () => {
+        const cursor = document.querySelector(".cursor")
+        cursor.style.display = "none";
+    }
+
+    const test = (bool) => {
+        const div =  document.querySelector(".divtest");
+        div.addEventListener("mousemove", (e) => {
+            console.log(e.clientX)
+            div.style.backgroundColor = "red";
+        })
+    }
+
+    const mouseMove = (e) => {
+        //console.log(e.clientX, e.clientY)
+       // console.log(boxRef);
+       const cursor = document.querySelector(".cursor")
+        
+
+    }
+
+    const handleMouseHover = (bool, num) => {
+        setIsHovering(bool)
+        if(bool === true ){
+            setBackground(tabImg[num-1]);
+        }
+      };
    
 
     return (
-        <div className="container" >
+        <div className="container" onMouseMove={mouseMove} >
             <div className="cursor" style={{
-                opacity: 0.5,
-                zIndex: 5,
-                backgroundImage: `url(${image2})`,
+                opacity: 1,
+                backgroundImage: `url(${background})`,
                 left: cursorX + "px",
                 top: cursorY + "px"
             }}></div>
-                 {data.map(elt => <div className="inList" onMouseOver={() => changeImage(elt.title[elt.title.length-1])}><h1 className={`pouet${elt.title[elt.title.length-1]}`} >{elt.title}</h1></div>)}
-             
+
+                 {/* {data.map(elt => <div className="inList" onMouseMove={(e) => changeImage(e, elt.title[elt.title.length-1])} ><h1 className={`pouet${elt.title[elt.title.length-1]}`} >{elt.title}</h1></div>)} */}
+                 {data.map(elt => <div className="inList"  ref={boxRef} onMouseEnter={() => handleMouseHover(true, elt.title[elt.title.length-1])}
+          onMouseLeave={() => handleMouseHover(false)} ><h1 className={`pouet${elt.title[elt.title.length-1]}`}  >{elt.title}</h1> <div className="hover-reveal">
+        <div className="hover-reveal__inner">
+            {isHovering === true && <div className="hover-reveal__img" style={{ backgroundImage: `url(${background})`}}></div>}
+        </div>
+    </div>   </div>)}
+                 {/* {data.map(elt => <div className="inList" onMouseEnter={changeImage(true)} onMouseLeave={changeImage(false)}><h1 className={`pouet${elt.title[elt.title.length-1]}`} >{elt.title}</h1></div>)} */}
+            <div className="divtest"  ><button >Test</button></div>
              <div className="cursor2" style={{
                 left: cursorX + 85 + "px",
-                top: cursorY + 135 + "px"
+                top: cursorY + 135 + "px",
             }}></div>
-             <img src={image1} />
+             <img style={{display: "none"}} src={image1} />
         </div>
     );
 }

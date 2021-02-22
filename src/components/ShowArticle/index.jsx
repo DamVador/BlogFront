68890887dfa35da55e65fbd77222fc8c7e05d225
Damien <damienbanville@hotmail.fr>
2021-02-22 10:28:24 +0100

    front issue on home page fixed and show article page fetching datas

--- src/components/ShowArticle/index.jsx
+++ src/components/ShowArticle/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {useParams} from "react-router-dom";
+import './index.css';
 
 const ShowArticle = () => {
     const [title, setTitle] = useState("pas de titre");
@@ -17,22 +18,25 @@ const ShowArticle = () => {
         return response.json()
         })
         .then((response) => {
-            console.log(response)
+             console.log(response);
+            setTitle(response.title);
+            setContent(response.content);
         }).catch(error => {
         console.log(error)
         })
     }
 
     useEffect(() => {
+        console.log("dans le use effect")
         console.log(articleId)
         callFetch();
     }, [])
 
 
     return (
-        <div>
-            {title}
-            {content}
+        <div className="showArticle">
+            <h1 className="articleTitle" >{title}</h1>
+            <p className="content">{content}</p>
         </div>
     );
 }
